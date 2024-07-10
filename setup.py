fe8856daf9f4bb1c086bef03d991ac73b9dd847b
Eric Jang <eric@1x.tech>
2024-07-09 19:04:28 -0700

    install requirements on pip install -e

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import find_packages, find_namespace_packages, setup
+from setuptools import find_namespace_packages, setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+def parse_requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
 
 setup(
     name="magvit2",
     version="1.0",
     packages=find_namespace_packages(include=['taming.*']),
+    install_requires=parse_requirements('requirements.txt'),
 )
