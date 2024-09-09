330f468df4891022d14a2a7e5068f8d6d842a39d
vinyesm <vinyes.marina@gmail.com>
2024-09-09 10:58:45 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [
+            line.strip() for line in f
+            if line and not line.startswith('#') and '--index-url' not in line
+        ]
+
+def install_special_dependencies():
+    os.system('pip install torch==2.1.1 --index-url https://download.pytorch.org/whl/cu118')
+    os.system('pip install torchvision==0.16.1 --index-url https://download.pytorch.org/whl/cu118')
+
+setup(
+    name="open_magvit2",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description='Open-source replication of Google\'s MAGVIT-v2 tokenizer',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  
+    url="https://github.com/TencentARC/Open-MAGVIT2",
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
